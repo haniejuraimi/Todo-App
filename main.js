aa90937833b01214a6cbf0c83c924acf37d0c328
haniejuraimi <haniealia08@gmail.com>
2019-08-17 11:09:28 +0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,39 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+      todos: [],
+      newTodoName: ""
+    },
+    created: function() {
+        if (localStorage.getItem("todos")) {
+            this.todos = JSON.parse(localStorage.getItem("todos"));
+        }
+    },
+    methods: {
+        addTodo: function() {
+            const newTodo = {
+                name: this.newTodoName,
+                isDone: false
+            };
+
+            this.todos.unshift(newTodo);
+
+            this.newTodoName= "";
+
+            this.persistData();
+        },
+
+        deleteTodo: function(index) {
+            this.todos.splice(index, 1);
+        },
+
+        toggleDone: function(index) {
+            this.todos[index].isDone= !this.todos[index].isDone;
+        },
+
+        persistData: function() {
+            localStorage.setItem("todos", JSON.stringify(this.todos));
+        }
+    }
+
+  })
